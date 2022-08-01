9fa6178511afeec11b141ea4288ad2caf4bff452
albaur <albaur@student.42.fr>
2022-08-01 21:45:15 +0200

    moved from public to private in phonebook class

--- cppmodules/module0/ex01/includes/PhoneBook.hpp
+++ cppmodules/module0/ex01/includes/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: albaur <albaur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 13:51:40 by albaur            #+#    #+#             */
-/*   Updated: 2022/08/01 21:37:50 by albaur           ###   ########.fr       */
+/*   Updated: 2022/08/01 21:44:03 by albaur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,19 @@ class PhoneBook
 	size_t	oldest;
 	Contact	contacts[8];
 
-	public:
-	PhoneBook(void);
-
-	void		add(void);
-	void		search(void);
-	int			getSize(void);
 	void		prompt(size_t i, Contact &contact);
 	void		printTable(void);
 	void		printInfo(size_t i);
 	void		printColumn(Contact contact, size_t i);
 	std::string	getOutput(std::string &str);
 	bool		checkInput(std::string &str);
+
+	public:
+	PhoneBook(void);
+
+	void		add(void);
+	void		search(void);
+	int			getSize(void);
 };
 
 #endif
