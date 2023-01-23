e0bd4c471330ced73a52b0979271b684a700f14d
albaur <albaur@student.42.fr>
2023-01-23 20:10:37 +0100

    removed temp test and fixed makefile

--- ft_containers/srcs/main.cpp
+++ ft_containers/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: albaur <albaur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 13:12:23 by albaur            #+#    #+#             */
-/*   Updated: 2023/01/23 18:46:17 by albaur           ###   ########.fr       */
+/*   Updated: 2023/01/23 19:35:53 by albaur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -149,40 +149,6 @@ void	cmp(const MAP &lhs, const MAP &rhs)
 	std::cout << "eq: " << (lhs == rhs) << " | ne: " << (lhs != rhs) << std::endl;
 	std::cout << "lt: " << (lhs <  rhs) << " | le: " << (lhs <= rhs) << std::endl;
 	std::cout << "gt: " << (lhs >  rhs) << " | ge: " << (lhs >= rhs) << std::endl;
-}
-
-
-#define T1 char
-#define T2 int
-#define _pair std::pair
-typedef _pair<const T1, T2> T3;
-void	test_ft_vector2(void)
-{
-	std::map<T1, T2> mp1;
-	std::map<T1, T2> mp2;
-
-	mp1['a'] = 2; mp1['b'] = 3; mp1['c'] = 4; mp1['d'] = 5;
-	mp2['a'] = 2; mp2['b'] = 3; mp2['c'] = 4; mp2['d'] = 5;
-
-	cmp(mp1, mp1); // 0
-	cmp(mp1, mp2); // 1
-
-	mp2['e'] = 6; mp2['f'] = 7; mp2['h'] = 8; mp2['h'] = 9;
-
-	cmp(mp1, mp2); // 2
-	cmp(mp2, mp1); // 3
-
-	printf("%p\n", &(++(++mp1.begin()))->second);
-	(++(++mp1.begin()))->second = 42;
-	printf("%p\n", &(++(++mp1.begin()))->second);
-
-	cmp(mp1, mp2); // 4
-	cmp(mp2, mp1); // 5
-
-	swap(mp1, mp2);
-
-	cmp(mp1, mp2); // 6
-	cmp(mp2, mp1); // 7
 }
 
 void	test_ft_vector(void)
@@ -363,8 +329,7 @@ int	main(int argc, char **argv)
 	if (atoi(argv[2]) == false)
 	{
 		std::cout << "Testing : ft::vector" << std::endl << std::endl;
-		//test_ft_vector();
-		test_ft_vector2();
+		test_ft_vector();
 		std::cout << "______________________________________________________________________________" << std::endl;
 		std::cout << "Testing : ft::stack" << std::endl << std::endl;
 		test_ft_stack();
