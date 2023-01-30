7243e869b764adec3172fb8118fedfdb2468e64b
Alexandre Baur <senolem22@gmail.com>
2023-01-30 17:15:50 +0100

    fixed wrong namespace in test

--- ft_containers/test/srcs/vector/iterators.cpp
+++ ft_containers/test/srcs/vector/iterators.cpp
@@ -6,7 +6,7 @@
 /*   By: albaur <albaur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 23:17:20 by albaur            #+#    #+#             */
-/*   Updated: 2023/01/25 23:36:28 by albaur           ###   ########.fr       */
+/*   Updated: 2023/01/30 16:00:31 by albaur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,15 +39,15 @@ void	test_ft_vector_iterators(void)
 
 void	test_std_vector_iterators(void)
 {
-	ft::vector<char>							vect;
-	ft::vector<char>::const_iterator			iter1;
-	ft::vector<char>::const_iterator			iter2;
-	ft::vector<char>::const_reverse_iterator	iter3;
-	ft::vector<char>::const_reverse_iterator	iter4;
+	std::vector<char>							vect;
+	std::vector<char>::const_iterator			iter1;
+	std::vector<char>::const_iterator			iter2;
+	std::vector<char>::const_reverse_iterator	iter3;
+	std::vector<char>::const_reverse_iterator	iter4;
 
 	for (size_t i = 0; i < 26; i++)
 		vect.push_back('A' + i);
-	print_ft_vector(vect);
+	print_std_vector(vect);
 	std::cout << *vect.begin() << std::endl;
 	std::cout << *(--vect.end()) << std::endl;
 	std::cout << *vect.rbegin() << std::endl;
