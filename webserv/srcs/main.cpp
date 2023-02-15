00adcc7e8aa4fb2c9cf5236d50be69b0fe42ebf5
senolem <albaur@42mulhouse.fr>
2023-02-15 15:25:15 +0100

    started webserv

---
+++ webserv/srcs/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albaur <albaur@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 15:16:31 by albaur            #+#    #+#             */
+/*   Updated: 2023/02/15 15:16:31 by albaur           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "webserv.hpp"
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		(void)argv;
+	}
+	else
+	{
+		std::cerr << "Invalid arguments. Usage: ./webserv [config path]" << std::endl;
+		return (1);
+	}
+	return (0);
+}
